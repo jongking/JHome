200f8c038fc749c60d4e2854735d08f3cf2990bc
jongking <498160093@qq.com>
2015-04-15 23:12:06 +0800

    提交一个httphelper和爬虫的helper,为开发漫画网站的爬虫做准备

---
+++ JHome/JHelper/Tests/CrawlerHelper_Fixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JHelper.WebCrawler;
+using NUnit.Framework;
+
+namespace JHelper.Tests
+{
+    [TestFixture]
+    class CrawlerHelper_Fixture
+    {
+        [Test]
+        public void Can_CrawlOver_Html()
+        {
+            var result = CrawlerHelper.CrawlOver(new Uri("http://manhua.dmzj.com/"));
+
+            Assert.AreNotEqual(result, null);
+        }
+    }
+}
---
+++ JHome/JHelper/WebCrawler/CrawlerHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JHelper.WebCrawler
+{
+    public static class CrawlerHelper
+    {
+        public static string CrawlOver(Uri uri)
+        {
+            HttpHelper http = new HttpHelper();
+            HttpItem item = new HttpItem {URL = uri.AbsoluteUri};
+            HttpResult result = http.GetHtml(item);
+            return result.Html;
+        }
+    }
+}
