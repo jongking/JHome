a2e02fd266863d6405646ac7678a77ad91396079
jongking <498160093@qq.com>
2015-04-27 23:13:31 +0800

    添加ClearScript的执行脚本库修改js编辑器的功能

---
+++ JHome/web/api/Crawler.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Crawler.aspx.cs" Inherits="api_Crawler" %>
---
+++ JHome/web/api/Crawler.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using JHelper;
+using Microsoft.ClearScript;
+using Microsoft.ClearScript.V8;
+using Microsoft.ClearScript.Windows;
+
+public partial class api_Crawler : GloPage
+{
+    public void RunJs()
+    {
+        var js = WebHelper.Request("JS", Page);
+        var jsResultWrap = new JsResultWrap();
+
+
+        using (ScriptEngine engine = new JScriptEngine())
+        {
+            //添加用于返回的result对象
+//            engine.AddHostObject("result", result);
+
+            engine.AddHostObject("jsResultWrap", jsResultWrap);
+            engine.Execute(js);
+        }
+
+        JsonResult.SetDateByClass(jsResultWrap);
+    }
+    public void GetJs()
+    {
+        var jsResultWrap = new JsResultWrap();
+        var serpath = Server.MapPath("../temp/comicCrawler/1.txt");
+        jsResultWrap.result = File.ReadAllText(serpath);
+        JsonResult.SetDateByClass(jsResultWrap);
+    }
+    public void SaveJs()
+    {
+        var js = WebHelper.Request("JS", Page);
+        var serpath = Server.MapPath("../temp/comicCrawler/1.txt");
+        File.WriteAllText(serpath, js);
+        JsonResult.SetDateByClass(new JsResultWrap());
+    }
+    public class JsResultWrap
+    {
+        public string result = "";
+    }
+
+    
+}
